c2a39d3d01e3a4098d0e3051415a3dea49261e95
federico claudi <federicoclaudi@eduroam-int-dhcp-97-130-13.ucl.ac.uk>
2018-11-13 14:00:08 +0000

    added test docstring and debugged math

--- Processing/tracking_stats/extract_velocities_from_tracking.py
+++ Processing/tracking_stats/extract_velocities_from_tracking.py
@@ -28,9 +28,13 @@ It also takes each pair of bps and calculates the distance between them, the ang
 """
 
 
-
-
 def extract_velocities_from_pose(pose):
+    '''extract_velocities_from_pose [see above]
+    
+    Arguments:
+        pose {[pd.DataFrane]} -- [multi layer dataframe for pose data from DLC]
+    '''
+
     # For each bodypart extract velocity and acceleration
     for bp in pose:
         distance_between_frames = calc_distance_between_points_in_a_vector_2d(bp.x.values, bp.y.values)
@@ -45,11 +49,31 @@ def extract_velocities_from_pose(pose):
     body_parts = pose.keys()
     body_segments = list(combinations(body_parts, 2))
 
-    for bp1, bp2 in body_segments:
+    for bp1_name, bp2_name in body_segments:
+        # get segment length, orientation and angular velocity
+        bp1, bp2 = pose[bp1_name], pose[bp2_name]
         bp1_pos = np.array([bp1.x.values, bp1.y.values])
         bp2_pos = np.array([bp2.x.values, bp2.y.values])
 
         segment_length = calc_distance_between_points_two_vectors_2d(bp1_pos, bp2_pos)
+        segment_angle = calc_angle_between_vectors_of_points_2d(bp1_pos, bp2_pos)
+        segment_angular_velocity = calc_ang_velocity(segment_angle)
+
+        # Add to pandas dataframe for pose
+        # ! I don't know how to do this
+        # ? create layerd dataframe: pose > segmnet > [length, orientation, ang vel]
+        segment_name = '{}-{}'.format(bp1_name, bp2_name)
+        pose[segment_name] = 'aaa'   # First layer of pandas dataframe
+        # with sublayers: length, angle, angular velocity
+        pose[segment_name]['length'] = segment_length
+        # ...
+
+
+
+
+
+
+
 
         
 
--- Processing/tracking_stats/math_utils.py
+++ Processing/tracking_stats/math_utils.py
@@ -2,27 +2,46 @@ import numpy as np
 from scipy import misc
 import pandas as pd
 from scipy.spatial import distance
-from math import factorial, atan2, degrees
+from math import factorial, atan2, degrees, acos, sqrt, pi
 import math
-from math import acos
-from math import sqrt
-from math import pi
 
 
 def calc_distance_between_points_2d(p1, p2):
+    '''calc_distance_between_points_2d [summary]
+    
+    Arguments:
+        p1 {[list, array]} -- [X,Y for point one]
+        p2 {[list, array]} -- [X,Y for point two]
+    
+    Returns:
+        [float] -- [eucliden distance]
+
+    Test: - to check : print(zero, oneh, negoneh)
+    >>> zero = calc_distance_between_points_2d([0, 0], [0, 0])
+    >>> oneh = calc_distance_between_points_2d([0, 0], [100, 0])
+    >>> negoneh = calc_distance_between_points_2d([-100, 0], [0, 0])
+    '''
+
     return distance.euclidean(p1, p2)
 
 
-def calc_distance_between_points_in_a_vector_2d(v1, v2):
-    """
-    Calculates the euclidean distance between consecutive point in a 2d array
+def calc_distance_between_points_in_a_vector_2d(v1):
+    '''calc_distance_between_points_in_a_vector_2d [for each pairwise p1,p2 in the two vectors get distnace]
+    
+    Arguments:
+        v1 {[np.array]} -- [2d array, X,Y position at various timepoints]
+    
+    Raises:
+        ValueError -- [description]
+    
+    Returns:
+        [np.array] -- [1d array with distance at each timepoint]
 
-    v1, v2 = 1d numpy arrays with X and Y position at each timepoint
-    can handle pandas dataseries too
-    """
-
+    >>> v1 = [0, 10, 25, 50, 100]
+    >>> d = calc_distance_between_points_in_a_vector_2d(v1)
+    '''
     # Check data format
-    if isinstance(v1, list) or isinstance(v2, list) or isinstance(v1, dict) or isinstance(v2, dict):
+    if isinstance(v1, dict) or not np.any(v1) or v1 is None:
             raise ValueError(
                 'Feature not implemented: cant handle with data format passed to this function')
 
@@ -31,10 +50,9 @@ def calc_distance_between_points_in_a_vector_2d(v1, v2):
         v1, v2 = v1.values, v2.values
     except:  # all good
         pass
-
     # loop over each pair of points and extract distances
     dist = []
-    for n, pos in enumerate(zip(v1, v2)):
+    for n, pos in enumerate(v1):
         # Get a pair of points
         if n == 0:  # get the position at time 0, velocity is 0
             p0 = pos
@@ -43,11 +61,7 @@ def calc_distance_between_points_in_a_vector_2d(v1, v2):
             p1 = pos  # get position at current frame
 
             # Calc distance
-            try:
-                dist.append(np.abs(distance.euclidean(p0, p1)))
-            except:
-                if np.isnan(p1).any():
-                    dist.append(np.nan)
+            dist.append(np.abs(distance.euclidean(p0, p1)))
 
             # Prepare for next iteration, current position becomes the old one and repeat
             p0 = p1
@@ -56,23 +70,37 @@ def calc_distance_between_points_in_a_vector_2d(v1, v2):
 
 
 def calc_distance_between_points_two_vectors_2d(v1, v2):
-    """
-    Calculates the euclidean distance between pair of points in two 2d arrays
+    '''calc_distance_between_points_two_vectors_2d [pairwise distance between vectors points]
+    
+    Arguments:
+        v1 {[np.array]} -- [description]
+        v2 {[type]} -- [description]
+    
+    Raises:
+        ValueError -- [description]
+        ValueError -- [description]
+        ValueError -- [description]
+    
+    Returns:
+        [type] -- [description]
 
-    v1, v2 = 2d numpy arrays, each with X and Y position at each timepoint
-    """
-
+    testing:
+    >>> v1 = np.zeros((2, 5))
+    >>> v2 = np.zeros((2, 5))
+    >>> v2[1, :]  = [0, 10, 25, 50, 100]
+    >>> d = calc_distance_between_points_two_vectors_2d(v1.T, v2.T)
+    '''
     # Check dataformats
-    if not isinstance(v1, np.ndarray()) or not isinstance(v2, np.ndarray()):
+    if not isinstance(v1, np.ndarray) or not isinstance(v2, np.ndarray):
         raise ValueError('Invalid argument data format')
-    if not v1.shape[0] == 2 or not v2.shape[0] == 2:
+    if not v1.shape[1] == 2 or not v2.shape[1] == 2:
         raise ValueError('Invalid shape for input arrays')
-    if not v1.shape[1] == v2.shape[1]:
+    if not v1.shape[0] == v2.shape[0]:
         raise ValueError('Error: input arrays should have the same length')
 
     # Calculate distance
     dist = distance.cdist(v1, v2, 'euclidean')
-
+    dist = dist[0, :]  # ! <-- need to check this behaviour
     return dist
 
 
@@ -88,13 +116,11 @@ def angle_between_points_2d_clockwise(p1, p2):
     Returns:
         [int] -- [clockwise angle between p1, p2 using the inner product and the deterinant of the two vectors]
 
-    Testing:
-        >>> zero = angle_clockwise([0, 1], [0, 1])
-        >>> ninety = angle_clockwise([1, 0], [0, 1])
-        >>> oneeighty = angle_clockwise([0, -1], [0, 1])
-        >>> twoseventy = angle_clockwise([-1, 0], [0, 1])
-        >>> error = angle_clockwise('a', 'b')
-        >>> print(zero, ninety, oneeighty, twoseventy)
+    Testing:  - to check:     print(zero, ninety, oneeighty, twoseventy)
+        >>> zero = angle_between_points_2d_clockwise([0, 1], [0, 1])
+        >>> ninety = angle_between_points_2d_clockwise([1, 0], [0, 1])
+        >>> oneeighty = angle_between_points_2d_clockwise([0, -1], [0, 1])
+        >>> twoseventy = angle_between_points_2d_clockwise([-1, 0], [0, 1])
     '''
 
     def length(v):
@@ -120,7 +146,7 @@ def angle_between_points_2d_clockwise(p1, p2):
     return inner
 
 
-def calc_angle_between_vectors_of_points_2d(v1, v2,):
+def calc_angle_between_vectors_of_points_2d(v1, v2):
     '''calc_angle_between_vectors_of_points_2d [calculates the clockwise angle between each set of point for two 2d arrays of points]
     
     Arguments:
@@ -129,10 +155,18 @@ def calc_angle_between_vectors_of_points_2d(v1, v2,):
 
     Returns:
         [np.ndarray] -- [1d array with clockwise angle between pairwise points in v1,v2]
+    
+    Testing:
+    >>> v1 = np.zeros((2, 4))
+    >>> v1[1, :] = [1, 1, 1, 1, ]
+    >>> v2 = np.zeros((2, 4))
+    >>> v2[0, :] = [0, 1, 0, -1]
+    >>> v2[1, :] = [1, 0, -1, 0]
+    >>> a = calc_angle_between_vectors_of_points_2d(v2, v1)
     '''
 
     # Check data format
-    if v1 is None or v2 is None or not isinstance(v1, np.ndarray()) or not isinstance(v2, np.ndarray()):
+    if v1 is None or v2 is None or not isinstance(v1, np.ndarray) or not isinstance(v2, np.ndarray):
         raise ValueError('Invalid format for input arguments')
     if len(v1) != len(v2): 
         raise ValueError('Input arrays should have the same length')
@@ -140,29 +174,38 @@ def calc_angle_between_vectors_of_points_2d(v1, v2,):
         raise ValueError('Invalid shape for input arrays')
 
     # Calculate
-    angs = np.zeros(len(v1))
-    for i, (p1, p2) in enumerate(zip(v1, v2)):
+    n_points = v1.shape[1]
+    angs = np.zeros(n_points)
+    for i in range(v1.shape[1]):
+        p1, p2 = v1[:, i], v2[:, i]
         angs[i] = angle_between_points_2d_clockwise(p1, p2)
 
     return angs
 
-def calc_ang_velocity(orientation, fps: int=False):
-    """
-    Given a vector of orientation (degrees) per frame, calculates the velocity as either degrees per frame
-    or degrees per second (if fps != False).
 
-    :param orientation: vector of angle values
-    :param fps:  frame rate of video the orientation was extracted from
-    :return: angular velocity as either deg per sec or deg per frame.
-    """
-    rad_ori = np.radians(orientation.values)
-    rad_ang_vel = np.insert(np.diff(np.unwrap(rad_ori)), 0, 0)
+def calc_ang_velocity(angles):
+    '''calc_ang_velocity [calculates the angular velocity ]
+    
+    Arguments:
+        angles {[np.ndarray]} -- [1d array with a timeseries of angles in degrees]
+    
+    Returns:
+        [np.ndarray] -- [1d array with the angular velocity in degrees at each timepoint]
+    
+    testing:
+    >>> v = calc_ang_velocity([0, 10, 100, 50, 10, 0])    
+    '''
+    # Check input data
+    if angles is None or not np.any(angles):
+        raise ValueError('Invalid input data format')
+    if not isinstance(angles, np.ndarray) and not isinstance(angles, list):
+        raise ValueError('Invalid input data format')
 
-    if not fps:    # return and vel as degrees per frame
-        return np.degrees(rad_ang_vel)
-    else:          # return and vel as degrees per sec
-        return np.degrees(np.multiply(rad_ang_vel, fps))
-
+    # Calculate
+    angles_radis = np.radians(angles) # <- to unwrap
+    ang_vel_rads = np.insert(np.diff(np.unwrap(angles_radis)), 0, 0)
+    return np.degrees(ang_vel_rads)
+    
 
 if __name__ == "__main__":
     import doctest
