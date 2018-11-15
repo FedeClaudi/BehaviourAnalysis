ae8e3c492b5c4fd5583bc80f6e59bae5ac8b790a
Federico Claudi <federicoclaudi@gmail.com>
2018-11-15 16:48:17 +0000

    small changes

--- Processing/rois_toolbox/get_maze_components.py
+++ Processing/rois_toolbox/get_maze_components.py
@@ -3,6 +3,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
 def get_rois_from_templates():
     pass
@@ -17,27 +18,38 @@ def get_maze_configuration_transitions(video_path):
     prev_frame = None
     pixel_difference = []
     print('Processing')
+    framen = 0
     while True:
         ret, frame = cap.read()
         if not ret: break
+        framen += 1
+        print(framen)
+
+        if not framen % 10 == 0: continue
+
+        if framen < 10000: 
+            continue
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        mean_color = np.mean(gray)
+        # gray = cv2.threshold(gray, mean_color+100, 255, cv2.THRESH_BINARY)[1]
+
         if prev_frame is None:
             prev_frame = gray
-            pixel_difference.append(0)
         else:
             diff = cv2.absdiff(prev_frame, gray)
-            pixel_difference.append(np.sum(diff))
+            thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1]
+            pixel_difference.append(np.mean(thresh))
             prev_frame = gray
+
     print('Processed: {} frames'.format(len(pixel_difference)))
     plt.plot(pixel_difference)
     plt.show()
 
 
 if __name__ == "__main__":
-    test_file = 'Users/federicoclaudi/Desktop/test.mp4'
+    test_file = 'D:\\Dropbox (UCL - SWC)\\Dropbox (UCL - SWC)\\Rotation_vte\\raw_data\\to_sort\\181114_CA3155_1\\cam1.avi'
     # os.chd('Users/federicoclaudi/Desktop')
     get_maze_configuration_transitions(test_file)
-
 
 
