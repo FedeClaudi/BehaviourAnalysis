030c6be8db1876f470332140b8fb018b86402062
federico claudi <federicoclaudi@federicos-MacBook-Air.local>
2018-11-26 09:47:13 +0000

    added files to move

--- Tracking/tracking.py
+++ Tracking/tracking.py
@@ -10,7 +10,12 @@ from Utilities.file_io.files_load_save import load_yaml
 
 class SetUpTracking:
     def __init__(self, rawfolder, config_file, processedfolder=None, given_list=None, overwrite=False):
-        if processedfolder is None: processedfolder = rawfolder
+        if processedfolder is None: 
+            processedfolder = rawfolder
+            need_to_move = False  # ? Need to move .h5 and .pickle files to new folder after tracking
+        else:
+            need_to_move = True
+
 
         self.raw = rawfolder
         self.proc = processedfolder
@@ -32,7 +37,16 @@ class SetUpTracking:
         print('Analysing {} videos with DeepLabCut'.format(len(to_process_videos)))
         deeplabcut.analyze_videos(self.cfg, [os.path.join(self.raw, f) for f in to_process_videos], gputouse=0, save_as_csv=False)
 
-       
+        # Move 
+        if need_to_move:
+            files_to_move = [f for f in os.listdir(self.raw) if '.h5' in f or '.pickle' in f]
+            if len(files_to_move) != 2*len(to_process_videos):
+                raise ValueError('Something went wrong when trying to move files')
+            print('Moving {} files'.format(len(files_to_move)))
+            
+            for file in files_to_move:
+                os.rename(os.path.join(self.raw, file), os.path.join(self.proc, file))
+
 if __name__ == "__main__":
     paths = load_yaml('paths.yml')
 
