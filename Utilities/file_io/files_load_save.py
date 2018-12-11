4efd3de4634962177ff09ba35dad84b0b157e38f
federico claudi <federicoclaudi@gmail.com>
2018-12-11 11:07:22 +0000

    added subprocess file move

--- Utilities/file_io/files_load_save.py
+++ Utilities/file_io/files_load_save.py
@@ -1,12 +1,19 @@
 import yaml
 import shutil
 import os
+import subprocess
+import sys
 
 def load_yaml(file):
         if not isinstance(file, str): raise ValueError('Invalid input argument')
         with open(file, 'r') as f:
                 loaded = yaml.load(f)
         return loaded
+
+
+def copyWithSubprocess(cmd):
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
 
 
 def load_tdms_from_winstore(filetomove):
@@ -16,7 +23,20 @@ def load_tdms_from_winstore(filetomove):
         if name in os.listdir(temp_dest):
                 print('File was already there')
         else:
-                # shutil.copy(filetomove, dest)
-                os.system('xcopy"{}""{}"'.format(filetomove, dest))
+                # ? python file copying is slow, using subprocess instead
+                # ? but suprocess is platform dependant so get right command 
+                # ! shutil.copy(filetomove, dest)
+
+                if sys.platform.startswith("win"):
+                        cmd = ['xcopy', filetomove, dest, '/K/O/X']
+                else:
+                        cmd = ['cp', filetomove, dest]
+
+                copyWithSubprocess(cmd) # * moving here
+
                 print('Moved {} to {}'.format(filetomove, dest))
         return dest
+
+
+
+
