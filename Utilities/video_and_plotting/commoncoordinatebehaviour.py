6250ab459b08f8ab3fa40c57329d7b0a85d83a00
federico claudi <federicoclaudi@gmail.com>
2018-12-21 18:36:06 +0100

    small stuff

--- Utilities/video_and_plotting/commoncoordinatebehaviour.py
+++ Utilities/video_and_plotting/commoncoordinatebehaviour.py
@@ -17,7 +17,7 @@ def run(videopath, maze_model=None):
         # Get the maze model template
         maze_model = cv2.imread('Utilities\\video_and_plotting\\mazemodel.png')
         maze_model = cv2.resize(maze_model, (1000, 1000))
-        maze_model = cv2.cv2.cvtColor(maze_model,cv2.COLOR_RGB2GRAY)
+        maze_model = cv2.cvtColor(maze_model,cv2.COLOR_RGB2GRAY)
 
     # Define points to be used for alignemt
     points = np.array([[435, 290], [565, 290], [435, 710], [565, 710]])
@@ -76,9 +76,24 @@ def run(videopath, maze_model=None):
     return registration[0], points, top_pad, side_pad
 
 
+def define_roi_on_template():
+    maze_model = cv2.imread('Utilities\\video_and_plotting\\mazemodel.png')
+    if maze_model is None:
+        maze_model = cv2.imread('Utilities/video_and_plotting/mazemodel.png')
+    maze_model = cv2.resize(maze_model, (1000, 1000))
+    maze_model = cv2.cvtColor(maze_model, cv2.COLOR_RGB2GRAY)
+    r = cv2.selectROI(maze_model)
+    print('\n\nSelect ROI:\n    x - {}, width - {}\n    y - {}, hieght - {}'.format(
+                                    r[1], r[3], r[0], r[2]))
+    # cv2.imshow('mm', maze_model)
+    # while True:
+    #     cv2.waitKey(1)
+
 if __name__ == "__main__":
     testfile = 'Z:\\branco\\Federico\\raw_behaviour\\maze\\video\\180606_CA2762.avi'
-    run(testfile)
+    # run(testfile)
+    define_roi_on_template()
+    
 
 
 
