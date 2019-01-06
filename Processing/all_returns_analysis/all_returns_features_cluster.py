db6f2b725e22981d05189e6e0f0d70731b8bd720
federico claudi <federicoclaudi@gmail.com>
2019-01-06 15:56:22 +0000

    m

--- Processing/all_returns_analysis/all_returns_features_cluster.py
+++ Processing/all_returns_analysis/all_returns_features_cluster.py
@@ -42,29 +42,43 @@ class clusterer:
         self.fast, self.slow = self.prep_data()
         self.plotter_tester()
 
-    def prep_data(self,):
+    def prep_data(self):
+        def get_at_shelt(a):
+            try:
+                at_shelt = np.where(f <= 40)[0][0]
+            except:
+                return len(a)
+            else:
+                return at_shelt
+        
+        def get_trimmed_arrays(raw_data, idx):
+            d = raw_data['d'][:, idx]
+            at_shelt = get_at_shelt(d)
+
+            temp = np.zeros((3, len(d)))
+            temp[0, :at_shelt] = line_smoother_convolve(d[:at_shelt])
+            temp[1, :at_shelt] = line_smoother_convolve(raw_data['v'][:, idx])
+            temp[2, :at_shelt] = line_smoother_convolve(raw_data['o'][:, idx])
+            return temp
+
         # Get the 10 fastest and 10 slowest traces
         names, sel = ['d', 'v', 'o'], [4, 2, 3]
         raw_data = {n:self.ts.get_y(self.ts.data, sel=s)[0] for n,s in zip(names, sel)}
-        raw_data['o'][raw_data['o'] < 1] = np.nan
-        raw_data['o'][raw_data['o'] >359] = np.nan
-        # x, y = self.ts.get_y(self.ts.data, sel=0)[0], self.ts.get_y(self.ts.data, sel=1)[0]
-        # xy = np.array([x[:, 0], y])
-        # raw_data['o'] = calc_angle_between_points_of_vector(xy.T)
-
         _, idxs = self.ts.array_sorter(raw_data['d'], sel=4)
-        fast, slow = {}, {}
-        for n, raw in raw_data.items():
-            slow[n] = raw_data[n][:, idxs[:10]]
-            fast[n] = raw_data[n][:, idxs[-10:]]
+        fast, slow = np.zeros((3, raw_data['d'].shape[0], 10)), []
+        for i in range(10):
+            fast[:, :, i] = get_trimmed_arrays(raw_data, idxs[i])
+            slow[:, :, i] = get_trimmed_arrays(raw_data, idxs[-i])
+            
+
+        
         return fast, slow
 
     def plotter_tester(self):
         f, axarr = plt.subplots(1, 3)
-        for i, c in enumerate(list(self.fast.keys())):
-            axarr[i].plot(self.fast[c], color='r', alpha=.75)
-            axarr[i].plot(self.slow[c], color='k', alpha=.75)
-            axarr[i].set(title=c)
+        for i in range(3):
+            axarr[i].plot(self.fast[i, :, :], color='r', alpha=.75)
+            axarr[i].plot(self.slow[i, :, :], color='k', alpha=.75)
 
 
 if __name__ == "__main__":
