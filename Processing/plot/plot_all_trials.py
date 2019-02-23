02dd5b0543e972a33d694eea8800505fee88b9dd
Federico Claudi <federicoclaudi@gmail.com>
2019-02-23 22:51:43 +0000

    aa

--- Processing/plot/plot_all_trials.py
+++ Processing/plot/plot_all_trials.py
@@ -17,7 +17,7 @@ from Utilities.video_and_plotting.video_editing import Editor
 class PlotAllTrials:
     def __init__(self, select_escapes=True):
         self.trials = AllTrials()
-        self.save_fld = 'D:\\Dropbox (UCL - SWC)\\Rotation_vte\\plots\\all_trials'
+        self.save_fld = 'D:\\Dropbox (UCL - SWC)\\Rotation_vte\\plots\\funny'
 
         if select_escapes:
             self.escapes = 'true'
@@ -29,15 +29,18 @@ class PlotAllTrials:
         experiments = set(AllTrials.fetch('experiment_name'))
 
         # Get all trials for each experiment, regardless of the mouse they belong to
-        for exp in sorted(experiments):
-            trials = (AllTrials & "experiment_name='{}'".format(exp) & "is_escape='{}'".format(self.escapes)).fetch('tracking_data')
-            self.plot_trials(trials, exp)
+        for exp in sorted(experiments):           
+            trials, fps, number_of_trials, trial_number, rec_uid, stim_frames = \
+                (AllTrials & "experiment_name='{}'".format(exp) & "is_escape='{}'".format(self.escapes))\
+                            .fetch('tracking_data', 'fps','number_of_trials', 'trial_number', 'recording_uid', 'stim_frame')
+
+            if not np.any(fps): continue
+            self.plot_as_video(trials, exp, 250, rec_uid, stim_frames, None, number_of_trials, trial_number)
 
     def plot_by_session(self, as_video=False):
         sessions = set(AllTrials.fetch('session_uid'))
 
         for uid in sessions:
-            things_to_fetch = ['experiment_name', 'tracking_data', 'fps', 'number_of_trials', 'trial_number', 'recording_uid', 'stim_frame']
             experiments, trials, fps, number_of_trials, trial_number, rec_uid, stim_frames = \
                 (AllTrials & "session_uid='{}'".format(uid) & "is_escape='{}'".format(self.escapes))\
                             .fetch('experiment_name', 'tracking_data', 'fps','number_of_trials', 'trial_number', 'recording_uid', 'stim_frame')
@@ -108,7 +111,7 @@ class PlotAllTrials:
 
         # loop over each trial
         stored_contours = []
-        for n, (tr, trial_number, rec_uid, stim_frame) in enumerate(zip(trials, trial_number, rec_uids, stim_frames)):
+        for n, (tr, trn, rec_uid, stim_frame) in enumerate(zip(trials, trial_number, rec_uids, stim_frames)):
             # shift all tracking Y up by 10px to align better
             trc = tr.copy()
             trc[:, 1, :] = np.add(trc[:, 1, :], 10)
@@ -183,26 +186,26 @@ class PlotAllTrials:
                 background = np.array(background[::-1, :, :])
 
                 # Title
-                if isinstance(number_of_trials, list):
-                    n_of_t = number_of_trials[n]
-                else:
-                    n_of_t = number_of_trials
+                # if isinstance(number_of_trials, list):
+                #     n_of_t = number_of_trials[n]
+                # else:
+                #     n_of_t = number_of_trials
 
-                if isinstance(exp, list):
-                    ttl = savename + ' - ' + exp[n]
-                else:
-                    ttl = savename
-                cv2.putText(background, ttl + '- trial ' + str(trial_number) + ' of ' + str(n_of_t),
-                            (int(maze_model.shape[1]/10), int(maze_model.shape[1]/10)), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 1,
-                            (255, 255, 255), 2, cv2.LINE_AA)
+                # if isinstance(exp, list):
+                #     ttl = savename + ' - ' + exp[n]
+                # else:
+                #     ttl = savename
+                # cv2.putText(background, ttl + '- trial ' + str(trn) + ' of ' + str(n_of_t),
+                #             (int(maze_model.shape[1]/10), int(maze_model.shape[1]/10)), 
+                #             cv2.FONT_HERSHEY_SIMPLEX, 1,
+                #             (255, 255, 255), 2, cv2.LINE_AA)
 
-                # Time elapsed
-                elapsed = frame / fps
-                cv2.putText(background, str(round(elapsed, 2)),
-                            (int(maze_model.shape[0]*.75), int(maze_model.shape[1]*.9)), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 2, 
-                            (255, 255, 255), 2,cv2.LINE_AA)
+                # # Time elapsed
+                # elapsed = frame / fps
+                # cv2.putText(background, str(round(elapsed, 2)),
+                #             (int(maze_model.shape[0]*.75), int(maze_model.shape[1]*.9)), 
+                #             cv2.FONT_HERSHEY_SIMPLEX, 2, 
+                #             (255, 255, 255), 2,cv2.LINE_AA)
 
                 # threat frame
                 threat = background[threat_cropping[0][0]:threat_cropping[0][1],
@@ -238,7 +241,6 @@ class PlotAllTrials:
             stored_contours.append(trial_stored_contours)
         
         writer.release()
-
 
 
 
@@ -372,12 +374,12 @@ class PlotAllTrials:
 
 if __name__ == "__main__":
     plotter = PlotAllTrials(select_escapes=True)
-    # plotter.plot_by_exp()
-    plotter.plot_by_session(as_video=True)
+    plotter.plot_by_exp()
+    # plotter.plot_by_session(as_video=True)
 
-    features_keys = load_yaml('Processing\\trials_analysis\\trials_observations.yml').keys()
-    for feature in features_keys:
-        plotter.plot_by_feature(feature)
+    # features_keys = load_yaml('Processing\\trials_analysis\\trials_observations.yml').keys()
+    # for feature in features_keys:
+    #     plotter.plot_by_feature(feature)
 
     # plotter.visualise_plots()
 
