c419bc20594d6e7efb7686b657fc1c060cb8384f
federico claudi <federicoclaudi@gmail.com>
2019-03-17 16:34:44 +0000

    s

--- Processing/modelling/bayesian/test.py
+++ Processing/modelling/bayesian/test.py
@@ -153,13 +153,15 @@ class Modeller:
         with pm.Model() as model:
             # p_sym = pm.Uniform("p_sym", 0, 1)
 
-            # sym_hyper = pm.Uniform("sym_hyper", 0, 1)
-            sym_prior = pm.Uniform("sym_prior", 0, 1, shape=len(sym_trials))
+            sym_hyper = pm.Uniform("sym_hyper", 0, 1)
+            sym_hyper2 = pm.Uniform("sym_hyper2", 0, 1)
+            sym_prior = pm.Uniform("sym_prior",  sym_hyper, sym_hyper2, shape=len(sym_trials))
             obs_sym = pm.Binomial('obs_sym', n=sym_trials, p=sym_prior, observed=sym_hits)
 
             # asym_hyper = pm.Uniform("asym_hyper", 0, 1)
-            asym_prior = pm.Uniform("asym_prior", 0, 1, shape=len(asym_trials))
-            obs_asym = pm.Binomial('obs_asym', n=asym_trials, p=asym_prior, observed=asym_hits)
+            # asym_hyper2 = pm.Uniform("asym_hyper2", 0, 1)
+            # asym_prior = pm.Uniform("asym_prior", asym_hyper, asym_hyper2, shape=len(asym_trials))
+            # obs_asym = pm.Binomial('obs_asym', n=asym_trials, p=asym_prior, observed=asym_hits)
 
 
             burned_trace = pm.sample(1000, tune=2000, nuts_kwargs={'target_accept': 0.95})
