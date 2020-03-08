4c7fb7ea204c5d4e1723daddac633b0bd5a05c89
FedeClaudi <federicoclaudi@protonmail.com>
2020-03-08 00:20:16 +0000

    bump

--- Analysis/simulations/ga_choice_evolution.py
+++ Analysis/simulations/ga_choice_evolution.py
@@ -6,15 +6,6 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 # %%
-#  Define params
-N_individuals = 1000
-N_generation = 200
-N_trials = 20
-
-KEEP_BEST_perc = 50
-KEEP_BEST = int((N_individuals/100)*KEEP_BEST_perc)
-
-# %%
 # Define distributions of path lengths and thethas
 lengths_distributions = npr.uniform(25, 75, size=5000)
 thethas_distributions = npr.uniform(25, 90, size=5000)
@@ -25,7 +16,7 @@ class Individual:
     speed = 10
     
     def __init__(self, idn):
-        self.genome = [npr.uniform(-1, 1), npr.uniform(-1, 1)]
+        self.genome = [npr.uniform(-3, 3), npr.uniform(-3, 3)]
 
         self.choices = []
         self.durations = []
@@ -41,10 +32,23 @@ class Individual:
     def __str__(self):
         return f'Individual {self.idn} - fitness: ' + str(self.fitness())
 
+#  Define params
+N_individuals = 500
+N_generation = 100
+N_trials = 40
+
+KEEP_BEST_perc = 33
+KEEP_BEST = int((N_individuals/100)*KEEP_BEST_perc)
+
 
 class Population:
     keep_best = KEEP_BEST
-    p_mutation = .05
+    p_mutation = .1
+
+    p_shortcut = [0.0, 0, 0, 0, 0, 0]#, 0.01, 0.05, .1, .5]
+    p_shortcut_idx = 0
+    change_p_short_every = 100
+    gen_num = 0
 
     inverted = False
 
@@ -66,11 +70,27 @@ class Population:
         self.max_id += 1
 
     def run_generation(self):
+        self.gen_num += 1
+
+        if self.gen_num > 0:
+            if self.gen_num % self.change_p_short_every == 0:
+                self.p_shortcut_idx +=1 
+        p_shortcut = self.p_shortcut[self.p_shortcut_idx]
+       
+
         for individual in self.pop:
             for trial in range(N_trials):
                 # ENVIRONMENT COMPUTATION
-                left = (choice(lengths_distributions), choice(thethas_distributions))
-                right = (choice(lengths_distributions), choice(thethas_distributions))
+                left = [choice(lengths_distributions), choice(thethas_distributions)]
+                right = [choice(lengths_distributions), choice(thethas_distributions)]
+
+                # apply shortcut
+                if npr.random() < p_shortcut:
+                    # only apply to one arm
+                    if npr.random() < .5:
+                        left[0] = left[0] - left[0]*np.cos(np.radians(left[1]))
+                    else:
+                        right[0] = right[0] - right[0]*np.cos(np.radians(right[1]))
 
                 # lengths factor
                 lf = (left[0] - right[0])/(left[0] + right[0])  # Will be positive if left longer right
@@ -80,26 +100,36 @@ class Population:
 
                 # AGENT COMPUTATIONS
                 # Compute choice
-                if not self.inverted:
-                    choice_factor = individual.genome[0] * lf + individual.genome[1] * af
-                else:
-                    choice_factor = individual.genome[1] * lf + individual.genome[0] * af
+                choice_factor = individual.genome[0] * lf #+ individual.genome[1] * af
 
                 # rescale to 0-1 range
                 choice_factor = (choice_factor + 1)/2
+                # TODO The probel is with this normalization, it doesn't work for all gene values
                 
                 # Choose an arm and compute escape duration
                 if npr.random() < choice_factor:
-                    individual.choices.append('r')
+                    chosen = 'r'
                     path = right
                 else:
-                    individual.choices.append('l')
+                    chosen = 'l'
                     path = left
+
+                if left[0] < right[0]:
+                    if chosen == 'l':
+                        individual.choices.append(1)
+                    else:
+                        individual.choices.append(0)
+                elif left[0] > right[0]:
+                    if chosen == 'r':
+                        individual.choices.append(1)
+                    else:
+                        individual.choices.append(0)
+                else:
+                    individual.choices.append(1)
 
                 individual.durations.append(path[0]/individual.speed)
 
         fitnesses = [ind.fitness() for ind in self.pop]
-
         sort_idx = np.argsort(fitnesses)
         self.pop = list(np.array(self.pop)[sort_idx])[::-1]
 
@@ -113,49 +143,49 @@ class Population:
             p2 = choice(parent_gen)
 
             son1 = Individual(self.max_id)
-            son1.genome[0] = p1.genome[0] if npr.random()>self.p_mutation else npr.uniform(-1, 1)
-            son1.genome[1] = p2.genome[1] if npr.random()>self.p_mutation else npr.uniform(-1, 1)
+            son1.genome[0] = p1.genome[0] if npr.random()>self.p_mutation else npr.uniform(-3, 3)
+            son1.genome[1] = p2.genome[1] if npr.random()>self.p_mutation else npr.uniform(-3, 3)
             self.add_to_population(son1)
 
             son2 = Individual(self.max_id)
-            son2.genome[0] = p2.genome[0] if npr.random()>self.p_mutation else npr.uniform(-1, 1)
-            son2.genome[1] = p1.genome[1] if npr.random()>self.p_mutation else npr.uniform(-1, 1)
+            son2.genome[0] = p2.genome[0] if npr.random()>self.p_mutation else npr.uniform(-3, 3)
+            son2.genome[1] = p1.genome[1] if npr.random()>self.p_mutation else npr.uniform(-3, 3)
             self.add_to_population(son2)
         
     def evolve(self):
-        max_fitness = []
+        mean_p_correct = []
+        mean_fitness = []
         best_individual = []
+        best_individual_p_correct = []
+
         mean_gene_0, mean_gene_1 = [], []
 
         for gen in tqdm(range(N_generation)):
+            mean_fitness.append(np.nanmean([i.fitness() for i in self.pop]))
+            mean_p_correct.append(np.nanmean([np.mean(i.choices) for i in self.pop]))
             self.run_generation()
             self.update_population()
 
             best_individual.append(self.pop[0])
-            max_fitness.append(self.pop[0].fitness())
+            best_individual_p_correct.append(np.nanmean(self.pop[0].choices))
             mean_gene_0.append(np.mean([i.genome[0] for i in self.pop]))
             mean_gene_1.append(np.mean([i.genome[1] for i in self.pop]))
 
-            if gen == 50:
-                self.inverted = True
-            if gen == 100:
-                self.inverted = False
-        return max_fitness, best_individual, mean_gene_0, mean_gene_1
+        return mean_fitness, mean_p_correct, best_individual, best_individual_p_correct, \
+                            np.array(mean_gene_0), np.array(mean_gene_1)
 
 
 
 # %%
 # Initialise individuals
 pop = Population()
-fitness, individuals, mean_gene_0, mean_gene_1 = pop.evolve()
-
-
+fitness, mean_p_correct, individuals, best_individual_p_correct, mean_gene_0, mean_gene_1 = pop.evolve()
 
 # Summary plots
 
-f, axarr = plt.subplots(nrows=3, sharex=True)
+f, axarr = plt.subplots(nrows=5, sharex=True, figsize=(16, 10))
 
-axarr[0].plot(fitness, label='Max fitness')
+axarr[0].plot(fitness, label='Mean fitness')
 
 axarr[1].plot([ind.genome[0] for ind in individuals], label="Gene 0")
 axarr[1].plot([ind.genome[1] for ind in individuals], label="Gene 1")
@@ -163,24 +193,22 @@ axarr[1].plot([ind.genome[1] for ind in individuals], label="Gene 1")
 axarr[2].plot(mean_gene_0, label="Gene 0")
 axarr[2].plot(mean_gene_1, label="Gene 1")
 
+axarr[3].plot((mean_gene_0-mean_gene_1)/(mean_gene_0+mean_gene_1), label="Gene 0/gene 1")
 
-axarr[0].legend()
-axarr[1].legend()
-axarr[2]
+axarr[4].plot(best_individual_p_correct, label='Best')
+axarr[4].plot(mean_p_correct, label='Mean')
 
-axarr[0].axvline(50)
-axarr[1].axvline(50)
-axarr[2].axvline(50)
+for ax in axarr:
+    ax.legend()
+    for v in [100, 200, 300]:
+        ax.axvline(v)
 
-axarr[0].axvline(100)
-axarr[1].axvline(100)
-axarr[2].axvline(100)
-
-axarr[0].set(title='Max fitness')
+axarr[0].set(title='Mean fitness')
 _ = axarr[1].set(title='Best genome')
 _ = axarr[2].set(title='Mean genome', xlabel='# generation')
+_ = axarr[3].set(title='Gene 0 / Gene 1')
+_ = axarr[4].set(title='Mean p(correct) [best individual]')
 
-
-
+f.tight_layout()
 
 # %%
